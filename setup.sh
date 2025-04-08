a4bb39150bd7825b813c63299a354e9d2f2ee16a
Rajitha Lakshan Wijesinghe <rajithawijesinghe74@gmail.com>
2025-04-08 22:20:04 +0530

    Initial commit with project structure

---
+++ setup.sh
@@ -0,0 +1,10 @@
+mkdir MoonInsurance
+cd MoonInsurance
+mkdir agent_service integration_service notification_service aggregator_service
+mkdir k8s
+mkdir .github
+mkdir .github/workflows
+mkdir observability
+mkdir data_generator
+mkdir docs
+git init
